ad7dad8b8fa77f9e3ec4e823ab077ef65e58a0ea
alphaaviral <alphaaviral@gmail.com>
2022-07-02 11:35:07 +0530

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+function getData(){
+    httpGetAsync(c);
+}
+
+function c(text){
+    const data = JSON.parse(text);
+    document.getElementById("div1").innerHTML = "First Dustbin : "+data.a;
+    document.getElementById("div2").innerHTML = "Second Dustbin : "+data.b;
+}
+
+
+function httpGetAsync(callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", 'https://flutter-f8ded-default-rtdb.firebaseio.com/Fm.json', true); // true for asynchronous 
+    xmlHttp.send(null);
+}
